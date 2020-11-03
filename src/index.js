e8634f4afb21062c8f2ba6f5631f7ce0f179ca8f
bmark88 <bradley.mark@hotmail.com>
2020-11-03 17:24:29 -0500

    Add redux persist  to cartReducer to ccapture cuser cartItems history

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // Styles
 import './index.css';
@@ -13,7 +14,9 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
