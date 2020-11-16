fc737a070c082ef62e9dec6626404bd67a692e9d
bmark88 <bradley.mark@hotmail.com>
2020-11-16 08:59:00 -0500

    Render size class conditionally on menu item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -7,7 +7,7 @@ import './menu-item.styles.scss';
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
     <div 
-      className={`${size} menu-item`}
+      className={`${size ? size + ' ' : ''}menu-item`}
       onClick={() => {history.push(`${match.url}${linkUrl}`)}}
     >
       <div 
